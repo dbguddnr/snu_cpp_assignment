7cf3deae3d517d8f2ceca03f6e4418766b0eb130
dbguddnr <90172427+dbguddnr@users.noreply.github.com>
2021-09-17 21:40:50 +0900

    Add files via upload

---
+++ mission 1/case 2/Project4/소스.cpp
@@ -0,0 +1,49 @@
+#include "std_lib_facilities.h"
+
+//자연수 n에 대하여 1부터 n의 절반까지의 수를 대입하여 소수인지 여부를 판단하기
+
+
+int prime(int x)
+{
+	int abx;
+	if (x >= 0)
+		abx = x;
+	else
+		abx = -x;
+
+
+	if (abx == 1 || abx == 0)
+	{
+		cout << x << "은(는) 소수가 아닙니다.\n";
+		return 0;
+	}
+	else
+	{
+		int count = 0;
+		for (int i = 1; i <= abx/2; i++)
+		{
+			if (abx % i == 0)
+				count++;
+		}
+		if (count == 1)
+			cout << x << "은(는) 소수입니다.\n";
+		else
+			cout << x << "은(는) 소수가 아닙니다.\n";
+
+		return 0;
+	}
+
+}
+
+int main()
+{
+	int number;
+	
+
+	cout << "정수를 입력하세요.\n";
+	cin >> number;
+
+	prime(number);
+
+	return 0;
+}
